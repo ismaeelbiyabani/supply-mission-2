838cad03f433b9f54a0ca929bfe6cba7527a23b4
ftg-gtf <67550388+ftg-gtf@users.noreply.github.com>
2020-10-15 18:00:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,9 +30,31 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
-	boxSprite=createSprite(400,610,5,5)
-	boxSprite.addImage(boxIMG)
-	boxSprite.scale=0.06
+	//boxSprite=createSprite(400,610,5,5)
+	//boxSprite.addImage(boxIMG)
+	//boxSprite.scale=0.06
+
+	boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
 
 	engine = Engine.create();
 	world = engine.world;
